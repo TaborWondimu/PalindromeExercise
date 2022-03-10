3b17695bb3eccac84eed8643ebe97c5ccc2187ff
Tabor Wondimu <csmajortabor@gmail.com>
2022-03-10 15:11:21 -0800

    Finished writting unit tests and Palindrome method

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+ public  class WordSmith
+    {
+    public bool ISAPalindrome(string str)
+        {
+         if (str == null)
+            {
+             return false;
+            }
+            /*   var reversed = "";
+            for(int i = str.Length -1; i >= 0; i--)
+               {
+                   reversed += str[i].ToString();
+               }
+             if( reversed == str)
+               {
+                   return true;
+               }
+               else
+               {
+                   return false;
+               }
+            */
+            // this is refractoring the code
+            return str.SequenceEqual(str.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData(null,false)]
+        [InlineData("hi", false)]
+        [InlineData("tttt", true)]
+        [InlineData("ttest", false)]
+     
+        public void Test1(string str,bool expected)
         {
+            // arrange
+            WordSmith input = new WordSmith();
 
+            //act
+            var actual = input.ISAPalindrome(str);
+
+            //assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
